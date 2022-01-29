04a6f80bc260167233bd2b7fb2fd655ef80f9ca4
Marek <markjohn666@gmail.com>
2022-01-29 18:10:00 +0100

    Create schema and get all questions

--- server/server.js
+++ server/server.js
@@ -3,24 +3,46 @@ const { graphqlHTTP } = require('express-graphql');
 const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLInt,
+    GraphQLBoolean,
+    GraphQLList
 } = require('graphql');
 const app = express();
+const questions = require('../questions.json');
+
+const QuestionType = new GraphQLObjectType({
+    name: 'Question',
+    description: 'This represents a single question',
+    fields: () => ({
+        id: { type: GraphQLString },
+        type: { type: GraphQLString },
+        min: { type: GraphQLInt },
+        max: { type: GraphQLInt },
+        title: { type: GraphQLString },
+        enabled: { type: GraphQLBoolean },
+        required: { type: GraphQLBoolean },
+    }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        questions: {
+            type: new GraphQLList(QuestionType),
+            description: 'List of all questions',
+            resolve: () => questions
+        }
+    })
+})
 
 const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World!'
-            }
-        })
-    })
+    query: RootQueryType
 })
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    graphiql: true,
+    graphiql: true
 }));
 app.listen(5000, () => console.log('Server is running...'));
